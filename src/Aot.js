da6a67b6f4f739d71b613935ab0560beb79c8bbd
shuu26 <sugarybombs@gmail.com>
2023-04-14 01:20:40 -0400

    added aot countdown

---
+++ src/Aot.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import './Aot.css';
+const Aot = () => {
+    let year = new Date().getFullYear();
+    /**
+    * AOT countdown difference:
+    */
+    const AOTdifference = +new Date(`08/26/${year}`) - +new Date();
+
+    const computeRemainingTime = (difference) => {
+        let RemainingTime = {};
+    
+        if (difference > 0) {
+          RemainingTime = {
+            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((difference / 1000 / 60) % 60),
+            seconds: Math.floor((difference / 1000) % 60)
+          };
+        }
+        return RemainingTime;
+    }
+
+    const [AOTcountDown, AOTsetCountDown] = useState(computeRemainingTime(AOTdifference));
+    const AOTcountDownComponents = [];
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          AOTsetCountDown(computeRemainingTime(AOTdifference));
+        }, 1000);
+    
+        return () => clearTimeout(timer);
+    });
+
+    Object.keys(AOTcountDown).forEach((interval) => {
+        if (!AOTcountDown[interval]) {
+          return;
+        }
+      
+        AOTcountDownComponents.push(
+          <span>
+            {AOTcountDown[interval]} {interval}{" "}
+          </span>
+        );
+    });
+
+    return(
+      <p className="aot-timerText">
+        {AOTcountDownComponents.length ? AOTcountDownComponents : <span>It is out!</span>}
+      </p>
+    );
+}
+
+export default Aot;
