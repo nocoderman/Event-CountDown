626e12920efd5601ff6bae9fb43b43c535de3555
shuu26 <sugarybombs@gmail.com>
2023-04-23 01:04:10 -0400

    finished sign up implementation

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import './styles/App.css';
 import Aot from "./components/Aot";
 import SignUp from "./components/SignUp";
 import HomePage from "./components/HomePage";
-
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link} from "react-router-dom";
 
-function App() {
+export const currentCredentials = React.createContext();
 
+function App() {
+  const credentialState = useState('');
+  
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<HomePage/>}/>
-          <Route exact path="/signup" element={<SignUp/>}/>
-        </Routes>
-      </Router>
+      <currentCredentials.Provider value={credentialState}>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<HomePage/>}/>
+            <Route exact path="/signup" element={<SignUp/>}/>
+          </Routes>
+        </Router>
+      </currentCredentials.Provider>
       {/* <div className="countdown-container">
         <div className="aot-container">
           <Aot/>
